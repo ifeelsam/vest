86a1a77551778534b410b3fcab32d67b5b60e41f
ifeelsam <Sanskarsharma9005@gmail.com>
2024-06-01 02:28:50 +0530

    repo changed

---
+++ app/(marketing)/_components/Heading.tsx
@@ -0,0 +1,13 @@
+const Heading = () => {
+    return ( 
+        <div className="max-w-3xl space-y-0">
+            <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+                your ideas can be stored here, and planned to bring it in reality.
+
+                Welcome to <span className="underline">Vest</span>
+            </h1>
+        </div>
+    );
+}
+ 
+export default Heading;
